91385e53a387270433795cb8ea605e9328c27fbb
John Hayes <hayesjohn54@yahoo.com>
2022-06-09 19:20:02 -0400

    commit tree 4673576ef08def8e09fad4babca97cea070f5674

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // code your solution here
+const record = [
+    {year: "2015", result: "W"},
+    {year: "2014", result: "N/A"},
+    {year: "2013", result: "L"},
+]
+
+// const daHeck = (array) => {
+//     array.find("W")
+// }
+
+// function year(item){
+//     return item.year
+// }
+
+// function result(item){
+//     return item.result
+// }
+
+
+// function superbowlWin(array){
+//     for(let item of array){
+//         if()
+
+//     }
+
+
+function superbowlWin(array){
+    for(let item of array){
+        if(item.result === "W"){
+            return item.year
+        }
+    }
+
+}
+
+// function superbowlWin(array){
+//     for(let item of array){
+//         array.find(item.result === "W")
+//         if(array){
+//             return item.year
+//         }         
+
+//     }
+// }
